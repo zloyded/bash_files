f44cd20e78893a657e93aa286fad85bd6058c3bc
binaryanomaly <reto.haeberli@comnode.net>
2015-03-06 23:10:05 +0100

    Separated config items

---
+++ bash_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Config section 1 = show otherwise leave empty
+
+
+# Greeter info sections config
+show_hostname="1"
+show_diskinfo="1"
+show_lastlogins=""
+show_aliases=""
+show_random_cmdinfo="1"
+
+
+# Prompt config
+show_reboot_info="1"
